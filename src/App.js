63c7a48d104232a1f19ecbedd3e90911c299af48
Reenta <reenta.kim@gmail.com>
2024-07-19 13:42:39 +0900

    noonaProject 리엑트 기초

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from "./component/Box";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let counter = 0;
+    const [counter2, setCounter2] = useState(0);
+
+    const increase = () => {
+        counter = counter + 1;
+        setCounter2(counter2 + 1);
+        console.log('counter: ', counter, 'counter2: ', counter2);
+    };
+
+    //1. 유저가 버튼을 클릭한다.
+    //2. counter+1해서 1이됨
+    //3. setState함수소출
+    //4. console.log실행됨
+    return (
+        <div>
+            <div>{counter}</div>
+            <div>state: {counter2}</div>
+            <button onClick={increase}>클릭!!</button>
+            <Box name="리사" num={1}/>
+            <Box name={"제니"} num={2}/>
+            <Box name={"린타"} num={3}/>
+        </div>
+
+    );
 }
 
 export default App;
